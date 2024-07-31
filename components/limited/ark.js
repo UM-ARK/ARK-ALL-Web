91d2b6a5ccdbe1692494d793ba906bb65f0f180c
Yanzhen Huang <huangyanzhen0108@163.com>
2024-07-31 21:13:02 +0800

    :bee: 優化下載按鈕數據代碼，減少冗餘

--- components/limited/ark.js
+++ components/limited/ark.js
@@ -10,39 +10,13 @@ import themeImg from '../../public/img/theme_small.png';
 
 import { useTranslation } from "react-i18next";
 import phones from "../../public/img/home_page/phones.png";
-import apple_logo from "../../public/img/home_page/company_logos/AppleLogo.png";
-import google_play_logo from "../../public/img/home_page/company_logos/GooglePlayLogo.png";
-import huawei_logo from "../../public/img/home_page/company_logos/HuaweiLogo.png";
-import android_logo from "../../public/img/home_page/company_logos/AndroidLogo.png";
+import { downloadBtnData } from "./common_data/download_btn_data";
 import { useRouter } from "next/router";
 // import appleLogo from "../../public/img/home_page/company_logos";
 
 const Ark = (props) => {
   const { t } = useTranslation();
   const router = useRouter();
-
-  const downloadBtnData = [
-    {
-      source: "App Store",
-      link: "https://apps.apple.com/us/app/um-all/id1636670554",
-      icon: apple_logo
-    },
-    {
-      source: "Play Store",
-      link: "https://play.google.com/store/apps/details?id=one.umall",
-      icon: google_play_logo
-    },
-    {
-      source: "HUAWEI",
-      link: "https://umall.one/static/release/app-release.apk",
-      icon: huawei_logo
-    },
-    {
-      source: "Android",
-      link: "https://umall.one/static/release/app-release.apk",
-      icon: android_logo
-    }
-  ];
 
   return (
     <React.Fragment>
---
+++ components/limited/common_data/download_btn_data.js
@@ -0,0 +1,27 @@
+import apple_logo from "../../../public/img/home_page/company_logos/AppleLogo.png";
+import google_play_logo from "../../../public/img/home_page/company_logos/GooglePlayLogo.png";
+import huawei_logo from "../../../public/img/home_page/company_logos/HuaweiLogo.png";
+import android_logo from "../../../public/img/home_page/company_logos/AndroidLogo.png";
+
+export const downloadBtnData = [
+    {
+        source: "App Store",
+        link: "https://apps.apple.com/us/app/um-all/id1636670554",
+        icon: apple_logo
+    },
+    {
+        source: "Play Store",
+        link: "https://play.google.com/store/apps/details?id=one.umall",
+        icon: google_play_logo
+    },
+    {
+        source: "Android",
+        link: "https://umall.one/static/release/app-release.apk",
+        icon: android_logo
+    },
+    {
+        source: "HUAWEI",
+        link: "https://umall.one/static/release/app-release.apk",
+        icon: huawei_logo
+    },
+];
