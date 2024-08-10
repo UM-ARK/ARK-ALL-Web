660bdd4662fcf96cf4cedcb084cea8a38aba6a67
Yanzhen Huang <huangyanzhen0108@163.com>
2024-08-11 02:39:33 +0800

    :bee: 規整首頁

--- pages/index.js
+++ pages/index.js
@@ -28,32 +28,6 @@ const Home = () => {
 
   const { t } = useTranslation();
 
-  const benefitOne = {
-    title: t("Take the Bus!"),
-    desc: t("THBdesc"),
-    image: BusImg,
-    bullets: [
-      {
-        title: t("THBbullets1-title"),
-        desc: t("THBbullets1-desc"),
-        icon: <FaceSmileIcon />,
-      },
-    ],
-  };
-
-  const benefitTwo = {
-    title: t("Club Activity Tracking"),
-    desc: t("CATdesc"),
-    image: EventImg,
-    bullets: [
-      {
-        title: t("CATbullets1-title"),
-        desc: t("CATbullets1-desc"),
-        icon: <DevicePhoneMobileIcon />,
-      },
-    ],
-  };
-
   const demoData = [
     {
       name: t('USRST_num'),
@@ -81,6 +55,34 @@ const Home = () => {
     },
   ];
 
+  const benefitData = [
+    {
+      title: t("Take the Bus!"),
+      desc: t("THBdesc"),
+      image: BusImg,
+      bullets: [
+        {
+          title: t("THBbullets1-title"),
+          desc: t("THBbullets1-desc"),
+          icon: <FaceSmileIcon />,
+        },
+      ],
+    },
+    {
+      title: t("Club Activity Tracking"),
+      desc: t("CATdesc"),
+      image: EventImg,
+      bullets: [
+        {
+          title: t("CATbullets1-title"),
+          desc: t("CATbullets1-desc"),
+          icon: <DevicePhoneMobileIcon />,
+        },
+      ],
+    }
+
+  ]
+
   return (
     <ARKMain withOutMargin={true}>
       <Head>
@@ -98,45 +100,59 @@ const Home = () => {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 0.5 }}>
+
+        {/** 大首頁 */}
         <Ark />
 
-        <SectionTitle
-          pretitle={t('User Stats')}
-          title={t(`USRST_title`)}>
-          {/*t("arkText")*/}
-        </SectionTitle>
+        {/** 用戶數據 */}
+        <section>
+          <SectionTitle
+            pretitle={t('User Stats')}
+            title={t(`USRST_title`)}>
+            {/*t("arkText")*/}
+          </SectionTitle>
 
+          <div className={`flex md:w-[40rem] mx-auto mt-10 mb-24 min-[901px]:flex-row max-[900px]:flex-col gap-5`}>
+            {demoData.map((data, index) => (
+              <div key={index} className={`flex flex-col gap-3 w-48 h-full mx-auto text-center items-center justify-center`}>
+                <div>
+                  <p className={`text-themeColor font-bold text-sm`}>{data.name}</p>
+                </div>
+                <data.icon className={`w-10 mx-auto text-themeColor`} />
+                <div className={`flex flex-row items-end font-bold text-themeColor`}>
+                  <p className={`text-3xl`}>{data.num}</p>
+                  <p className={`text-sm opacity-50`}>{data.unit}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </section>
 
-        <div className={`flex md:w-[40rem] mx-auto mt-10 mb-24 min-[901px]:flex-row max-[900px]:flex-col gap-5`}>
-          {demoData.map((data, index) => (
-            <div key={index} className={`flex flex-col gap-3 w-48 h-full mx-auto text-center items-center justify-center`}>
-              <div>
-                <p className={`text-themeColor font-bold text-sm`}>{data.name}</p>
-              </div>
-              <data.icon className={`w-10 mx-auto text-themeColor`} />
-              <div className={`flex flex-row items-end font-bold text-themeColor`}>
-                <p className={`text-3xl`}>{data.num}</p>
-                <p className={`text-sm opacity-50`}>{data.unit}</p>
-              </div>
-            </div>
+        {/* 好處 */}
+        <section>
+          <SectionTitle
+            pretitle={t("More Features")}
+            title={t("Why Use UM-ARK-ALL?")}>
+            {t("arkText")}
+          </SectionTitle>
+
+          {benefitData.map((v, k) => (
+            <Benefits key={k} data={v} imgPos={k % 2 == 0 ? "left" : "right"} />
           ))}
-        </div>
 
-        <SectionTitle
-          pretitle={t("More Features")}
-          title={t("Why Use UM-ARK-ALL?")}>
-          {t("arkText")}
-        </SectionTitle>
+        </section>
 
-        <Benefits data={benefitOne} />
-        <Benefits imgPos="right" data={benefitTwo} />
+        { /** FAQ */}
+        <section>
+          <SectionTitle
+            pretitle={t("FAQ")}
+            title={t("ARK ALL Frequently Asked Questions")}>
+            {t("Frequently Asked Questions")}
+          </SectionTitle>
+          <Faq />
+        </section>
 
-        <SectionTitle
-          pretitle={t("FAQ")}
-          title={t("ARK ALL Frequently Asked Questions")}>
-          {t("Frequently Asked Questions")}
-        </SectionTitle>
-        <Faq />
+        {/** 下載集合 */}
         <Cta />
       </motion.div>
 
