bbaad9c9e693efb6c048042afb4930e4b83bba54
Yanzhen Huang <huangyanzhen0108@163.com>
2024-08-13 00:44:53 +0800

    :bug: 修復Hamburger Button太大的問題

--- components/uiComponents/HamburgerBtn.js
+++ components/uiComponents/HamburgerBtn.js
@@ -8,10 +8,10 @@ export const HamburgerBtn = (props) => {
         setMobileMenuOpen(!m_isOpen);
     };
     return (
-        <div className={`mr-5 flex flex-col gap-0.5 lg:hidden hover:cursor-pointer`} onClick={handleClick}>
-            <div className={`w-5 bg-black h-[2.5px] rounded-full bg-trueGray-700 dark:bg-white ${m_isOpen && `rotate-45 translate-y-0.5`} transition-all`} />
-            <div className={`w-5 bg-black h-[2.5px] rounded-full bg-trueGray-700 dark:bg-white ${m_isOpen && 'hidden'} transition-all`} />
-            <div className={`w-5 bg-black h-[2.5px] rounded-full bg-trueGray-700 dark:bg-white ${m_isOpen && `-rotate-45 -translate-y-0.5`} transition-all`} />
+        <div className={`mr-5 flex flex-col gap-[3.5px] lg:hidden hover:cursor-pointer bg-[#ffffff50] p-4 rounded-xl`} onClick={handleClick}>
+            <div className={`w-8 bg-black h-[2.5px] rounded-full bg-trueGray-700 dark:bg-white ${m_isOpen && `rotate-45 translate-y-0.5`} transition-all`} />
+            <div className={`w-8 bg-black h-[2.5px] rounded-full bg-trueGray-700 dark:bg-white ${m_isOpen && 'hidden'} transition-all`} />
+            <div className={`w-8 bg-black h-[2.5px] rounded-full bg-trueGray-700 dark:bg-white ${m_isOpen && `-rotate-45 -translate-y-0.5`} transition-all`} />
         </div>
     );
 }
