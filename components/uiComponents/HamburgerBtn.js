7370f042ea359534279c4c53cb757a2db6132d04
Yanzhen Huang <huangyanzhen0108@163.com>
2024-08-13 00:57:23 +0800

    :bee: 優化Hamburger體驗

--- components/uiComponents/HamburgerBtn.js
+++ components/uiComponents/HamburgerBtn.js
@@ -9,9 +9,9 @@ export const HamburgerBtn = (props) => {
     };
     return (
         <div className={`mr-5 flex flex-col gap-[3.5px] lg:hidden hover:cursor-pointer bg-[#ffffff50] p-4 rounded-xl`} onClick={handleClick}>
-            <div className={`w-8 bg-black h-[2.5px] rounded-full bg-trueGray-700 dark:bg-white ${m_isOpen && `rotate-45 translate-y-0.5`} transition-all`} />
-            <div className={`w-8 bg-black h-[2.5px] rounded-full bg-trueGray-700 dark:bg-white ${m_isOpen && 'hidden'} transition-all`} />
-            <div className={`w-8 bg-black h-[2.5px] rounded-full bg-trueGray-700 dark:bg-white ${m_isOpen && `-rotate-45 -translate-y-0.5`} transition-all`} />
+            <div className={`w-6 bg-black h-[2.5px] rounded-full bg-trueGray-700 dark:bg-white ${m_isOpen && `rotate-45 translate-y-0.5`} transition-all`} />
+            <div className={`w-6 bg-black h-[2.5px] rounded-full bg-trueGray-700 dark:bg-white ${m_isOpen && 'hidden'} transition-all`} />
+            <div className={`w-6 bg-black h-[2.5px] rounded-full bg-trueGray-700 dark:bg-white ${m_isOpen && `-rotate-45 -translate-y-0.5`} transition-all`} />
         </div>
     );
 }
