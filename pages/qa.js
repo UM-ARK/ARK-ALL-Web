53d4fa5f4db2ade88cd03d0729419a8f259a4945
Rookie, LIN <1049825685@qq.com>
2026-07-31 19:11:45 +0800

    :sparkles: 增加中文版常見問題說明

--- pages/qa.js
+++ pages/qa.js
@@ -21,22 +21,23 @@ const qa = () => {
 
   const faqdata = [
     {
-      question: "我們組織想進駐 ARK ALL，要如何註冊呢？",
-      answer: `# 只要一封電郵即可，在此之前您還應閱讀和同意在我們網站對組織用戶的用戶協議。
-        聯絡郵箱：'umacark@gmail.com'
+      question: t("如何在APP首頁發佈活動？"),
+      answer: t("首頁是為澳大組織專設的活動公告欄，供同學集中獲取社團活動資訊。組織賬號申請可見下方說明，普通用戶可使用論壇發佈話題討論~"),
+    },
+    {
+      question: "如何註冊組織賬號，進駐 ARK ALL？",
+      answer: `請將註冊電郵發送到'umacark@gmail.com'，在此之前您還應閱讀和同意組織用戶的用戶協議。
 
-        Email 請包括以下內容：
+      Email 請至少包括以下內容：
+        組織名: [組織名]
+        賬號名: [賬號名]
+        登錄密碼: [登錄密碼]
+        組織類型: [組織類型]
+        組織 Logo: [附件]
+        組織證明: [組織證明]
+        發件人姓名
 
-        - 組織名: [組織名]
-        - 賬號名: [賬號名]
-        - 登錄密碼: [登錄密碼]
-        - 組織類型: [組織類型]
-        - 組織 Logo: [附件]
-        - 組織證明: [組織證明]
-        - 發件人姓名
-
-        # 說明: 
-        
+        說明: 
         - 組織名(自定義)。展示用途，不宜過長，中文更好，亦可以是大家熟知的簡稱。
           - 例如電腦學會，攝影學會等。
         - 賬號名(自定義)。在 ARK ALL 登錄時輸入。
@@ -56,17 +57,27 @@ const qa = () => {
         - 組織 Logo。正方形或圓形圖片，jpg/png。
         - 組織證明。任何一種可以證明貴組織是在澳大運作的組織的證明。
           - 可以是圖片、文件等。
-        - 發件人姓名。
         
-          請發件人使用澳大電郵
-          ("xxx@um.edu.mo" 或 "xxx@connect.um.edu.mo" 或 "xxx@umac.mo")
-          將以上註冊信息發送到 "umacark@gmail.com"。
+          請發件人使用澳大電郵發送
+          ("xxx@um.edu.mo" 或 "xxx@connect.um.edu.mo" 或 "xxx@umac.mo" 或 社團正在使用的Email)。
           
-          # 發件人的姓名和 UM ID 將被記錄。如有冒名、違反用戶協議等行為，則依法追究。`
+          發件人的姓名和 UM ID 將被記錄。如有冒名、違反用戶協議等行為，則依法追究。`
     },
     {
       question: t("Is ARK ALL free?"),
       answer: t("A1"),
+    },
+    {
+      question: t("ARK ALL 盈利嗎？"),
+      answer: t("ARK ALL是非盈利項目，可以直觀感受到APP的乾淨整潔，ARK ALL沒有收取任何廣告費用。\nARK ALL也是開放平台，也歡迎珠澳臨近的寶藏高質商戶/活動能宣傳到澳大師生群體中。發佈推廣前，請判斷是否適合澳大師生群體。"),
+    },
+    {
+      question: t("ARK ALL 的運作成本？"),
+      answer: t("目前每年淨支出約1,500RMB，未包含開發維護費用。每年純支出，0收入，希望各界人士多多捐贈支持！捐贈頁：https://github.com/UM-ARK/Donate"),
+    },
+    {
+      question: t("如何加入 ARK 開發團隊？"),
+      answer: t("隨時歡迎先，通過電腦學會 / GitHub / Email交流！我們歡迎各專業的同學在ARK中發光發熱，實現你的idea~ 打造全澳最強、廣東最強、中國最強校園APP！"),
     },
     {
       question: t("Does ARK ALL collect my password?"),
@@ -77,10 +88,14 @@ const qa = () => {
       answer: t("A3"),
     },
     {
+      question: t("ARK 系列產品組成"),
+      answer: t("截止2026年，目前ARK由APP: ARK ALL，Harbor論壇: harbor.umall.one，百科: wiki.umall.one，組織賬號系統: umall.one組成。"),
+    },
+    {
       question: t("ARK ALL's origin"),
-      answer: converter.makeHtml(`
-      ARK ALL 是 2022 年暑假，幾位不知名的 FST 同學奮戰兩個月做出來的，
-      整個應用全程都是由我們自主開發，其中都踩了不少坑，掉了不少頭髮 QAQ。  
+      answer: `
+      ARK ALL 是 2022 年暑假，由幾位 FST(ECE+CIS) 同學奮戰兩個月做出來的，
+      整個應用全程由澳大同學自主開發，踩了不少坑，掉了不少頭髮 QAQ。  
       
       ARK ALL 的前身是 ARK 微信小程式。
       該小程式已集成 ARK 學術分享會發佈、UM 校園巴士等功能，
@@ -88,17 +103,11 @@ const qa = () => {
       為如今的 ARK ALL 運作模式提供了寶貴的經驗。  
       
       小程式於 2021 年 9 月發佈，2022 年 4 月停運。
-      2個月後作者與這幾位不知名同學正式成立新 ARK 開發團隊
-      ARK ALL 項目正式立項開始開發。
-      `),
-    },
-    {
-      question: t("ARK ALL's Add Drop feature is not working properly."),
-      answer: "因為澳大系統的固有設計問題，例如 Add Drop 此類網頁會需要彈窗操作，盡量在電腦上操作會更便利。因此留下複製鏈接和在瀏覽器打開的功能。該功能主要是為新同學準備，初來乍到並不知道舊生所講的服務是何，ARK ALL 可以讓同學對澳大服務先有直觀的了解。",
-    },
-    {
-      question: t("Will ARK ALL have a desktop version?"),
-      answer: "Web、Mac，Windows 等一切皆有可能。但目前來講網頁版在緊張施工中，第一步先為組織賬號可在電腦使用的操作平台，更方便發佈資訊。接下來很有可能再將部分重要功能移植到 Web。But anyway，碼農作為新時代農民工，我們還是很希望有發燒友加入到我們的開發隊列中，共同向 ARK ALL 的目標邁進！",
+      2個月後正式成立新 ARK 開發團隊，ARK ALL 項目正式立項開始開發。
+      
+      2022/2023學年 Sem1 ARK ALL 1.0版本正式上線。
+      首個名稱是UM ALL，因為某些原因改名為ARK ALL。
+      `,
     },
     {
       question: t("I have feedback/suggestions to provide to the developers"),
